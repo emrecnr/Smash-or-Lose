9602bc996acd1cd1e169e038594ff48019b25319
Emre Çınar <121261363+platanusdev@users.noreply.github.com>
2023-05-05 21:47:32 +0300

    Ball Script Düzenlendi

--- Smash or Lose/Assets/Scripts/Ball/BallMovement.cs
+++ Smash or Lose/Assets/Scripts/Ball/BallMovement.cs
@@ -7,38 +7,81 @@ public class BallMovement : MonoBehaviour
 {
     Rigidbody2D rb2;
     private int scoreAmount= 1;
+    
+    [SerializeField]private int hitCounter;
+    [SerializeField] private float  initialSpeed=10;
+    [SerializeField] private float speedIncrease =0.25f;
+
 
     private void Start()
     {
+        
         rb2 = GetComponent<Rigidbody2D>();
-          float randomRange = Random.Range(0f, 2f);
-        if (randomRange <= 0.5f)
-        {
-            rb2.AddForce(new Vector2(80, 10));
-        }
-        else
-        {
-            rb2.AddForce(new Vector2(-80, -10));
-        }
+        
+        BallStartPos();  
 
     }
+    private void FixedUpdate()
+    {
+        rb2.velocity = Vector2.ClampMagnitude(rb2.velocity, initialSpeed+(speedIncrease*hitCounter));  
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.tag == "Player")
+        if (collision.gameObject.tag == "Player"||collision.gameObject.tag=="AI")
         {
-            Debug.Log("...");
-            rb2.velocity = new Vector2(rb2.velocity.x, rb2.velocity.y / 2 + collision.collider.GetComponent<Rigidbody2D>().velocity.y / 3);
+            PlayerBounce(collision.transform);
         }
+      
         if (collision.gameObject.tag =="Player Wall")
         {
             Debug.Log("Score Computer");
             ScoreControl.aiScore += scoreAmount;
+            ResetPosition();
             
         }
         if(collision.gameObject.tag =="AI Wall")
         {
             Debug.Log("Score Player!!");
             ScoreControl.playerScore +=scoreAmount;
+            ResetPosition();
         }
     }
+    private void BallStartPos()
+    {
+        rb2.velocity = new Vector2(-1, 0) * (initialSpeed + speedIncrease * hitCounter);
+    }
+    public void ResetPosition()
+    {
+        rb2.velocity = new Vector2 (0,0);
+        transform.position = new Vector2(0, 0);
+        hitCounter = 0;
+        Invoke("BallStartPos",2f);
+    }
+  
+    public void PlayerBounce(Transform myObject)
+    {
+        hitCounter++;
+        Vector2 ballPos = transform.position;
+        Vector2 playerPos = myObject.position;
+
+        float xDirection,yDirection;
+        if (transform.position.x>0)
+        {
+            xDirection = -1;
+
+        }
+        else
+        {
+            xDirection = 1;
+        }
+        yDirection = (ballPos.y - playerPos.y) / myObject.GetComponent<Collider2D>().bounds.size.y;
+        if (yDirection==0)
+        {
+            yDirection = 0.25f;
+        }
+        rb2.velocity = new Vector2(xDirection, yDirection) * (initialSpeed + (speedIncrease * hitCounter));
+    }
+    
+
 }
